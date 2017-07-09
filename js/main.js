9119bb4f031f7bb0925f3f18ab20296398de7322
Łuksz Strachanowski <lukasz.strachanowski@gmail.com>
2017-07-09 13:31:51 +0200

    scoreboard added

--- js/main.js
+++ js/main.js
@@ -4,22 +4,26 @@ $(document).ready(function() {
   player ="";
   allNumbers = [1,2,3,4,5,6,7,8,9];
   winCombinations = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-  
 
   $("#playerX").click(function() {
     $("#boardContainer").fadeIn(1500).removeClass("boardContainerHide");
+    $("#score").slideDown(500).removeClass("boardContainerHide");
     player = "x";
     $("#displayMessage").hide().html("You are playing as: <div class='selectedPlayer'>"+ player+ "</div>").slideDown(500);
-
   });
 
 
   $("#playerO").click(function() {
     $("#boardContainer").fadeIn(1500).removeClass("boardContainerHide");
+    $("#score").slideDown(500).removeClass("boardContainerHide");
     player ="o";
     $("#displayMessage").hide().html("You are playing as: <div class='selectedPlayer'>"+ player+ "</div>").slideDown(500);
   });
 
-
+  $("div.containerColor").click(function(){
+    if(player === "x"){
+      console.log(this.id);
+    }
+  });
 
 });
