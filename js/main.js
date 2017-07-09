2c110c9918795fb4441bc4f1510ad235938189ad
Łuksz Strachanowski <lukasz.strachanowski@gmail.com>
2017-07-09 12:23:15 +0200

    user can choose sign. Variables initializd

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
 
-
-  $("#boardContainer").addClass("boardContainerHide");
+  var player, allNumbers, winCombinations;
+  player ="";
+  allNumbers = [1,2,3,4,5,6,7,8,9];
+  winCombinations = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+  
 
   $("#playerX").click(function() {
-    $("#boardContainer").fadeIn("400", function() {
-      $("#boardContainer").addClass("boardContainerShow");
-    });
+    $("#boardContainer").fadeIn(1500).removeClass("boardContainerHide");
+    player = "x";
+    $("#displayMessage").hide().html("You are playing as: <div class='selectedPlayer'>"+ player+ "</div>").slideDown(500);
+
   });
 
 
   $("#playerO").click(function() {
-    $("#boardContainer").fadeIn("400", function() {
-      $("#boardContainer").addClass("boardContainerShow");
-    });
+    $("#boardContainer").fadeIn(1500).removeClass("boardContainerHide");
+    player ="o";
+    $("#displayMessage").hide().html("You are playing as: <div class='selectedPlayer'>"+ player+ "</div>").slideDown(500);
   });
+
 
 
 });
