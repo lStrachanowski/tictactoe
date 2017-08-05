758a82db22739e76bf92824170d471091597f990
Łuksz Strachanowski <lukasz.strachanowski@gmail.com>
2017-08-05 23:31:03 +0200

    game reset and continue options added, continue button is working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var player, comp, allNumbers, winCombinations, playerArr, compArr, turn, playerWinComb, compWinComb, blockNum,
-selectedLevel,currIdNum,currNum,scoreX,scoreY;
-blockNum = 0
+  selectedLevel, currIdNum, currNum, scoreX, scoreY;
 player = ""; //storing sign choosen by player
 comp = ""; //storing sign od computer
 turn = ""; //storing value , whos turn is it.
@@ -26,15 +25,15 @@ compWinComb = winCombinations.slice("");
 $(document).ready(function() {
   $("#score").html("x   " + scoreX + " : " + scoreO + "   o");
   //Easy level selected
-  $("#easyLevel").click(function(){
-    selectedLevel ="easy";
+  $("#easyLevel").click(function() {
+    selectedLevel = "easy";
     $("#displayMessage").fadeIn(500).removeClass("boardContainerHide");
     $("#levelBox").hide();
   });
 
   //Hard level selected
-  $("#hardLevel").click(function(){
-    selectedLevel ="hard";
+  $("#hardLevel").click(function() {
+    selectedLevel = "hard";
     $("#displayMessage").fadeIn(500).removeClass("boardContainerHide");
     $("#levelBox").hide();
   });
@@ -46,7 +45,7 @@ $(document).ready(function() {
     player = "x";
     comp = "o";
     turn = "x";
-    $("#displayMessage").hide().html("Level: " +selectedLevel + ".   You are playing as: <div class='selectedPlayer'>" + player + "</div>").slideDown(500);
+    $("#displayMessage").hide().html("Level: " + selectedLevel + ".   You are playing as: <div class='selectedPlayer'>" + player + "</div>").slideDown(500);
   });
 
   // selecting o as player singn
@@ -56,13 +55,35 @@ $(document).ready(function() {
     player = "o";
     comp = "x";
     turn = "o";
-    $("#displayMessage").hide().html("Level: " +selectedLevel + ".   You are playing as: <div class='selectedPlayer'>" + player + "</div>").slideDown(500);
+    $("#displayMessage").hide().html("Level: " + selectedLevel + ".   You are playing as: <div class='selectedPlayer'>" + player + "</div>").slideDown(500);
   });
+
+  $("#continue").click(function() {
+    playerArr = [];
+    compArr = [];
+    turn = "";
+    $(".containerColor").each(function(){
+      $("#" + this.id).html("");
+      $("#" + this.id).removeClass("selectedPlayer");
+    });
+    $("#new-link-line").remove();
+    allNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    playerWinComb = winCombinations.slice("");
+    compWinComb = winCombinations.slice("");
+    turn = player;
+    $("#gameOptionsContainer").fadeOut(500).addClass("boardContainerHide");
+  });
+
+    $("#reset").click(function() {
+
+  });
+
 
   $("div.containerColor").click(function() {
     // Is pushing clicked Id number to playerArr, which is storing ids clicked by player.
     currIdNum = (this.id).split("");
     currNum = Number(currIdNum[currIdNum.length - 1]);
+    console.log(currNum);
     if (allNumbers.indexOf(currNum) != -1) {
       // Is displaying x or o on the board.
       if (player === "x") {
@@ -72,7 +93,7 @@ $(document).ready(function() {
           compWinComb = searchMatchNumber(compWinComb, currNum);
           $("#" + this.id).html("x");
 
-          findBlockingNumber(playerWinComb, playerArr, 1,turn);
+          findBlockingNumber(playerWinComb, playerArr, 1, turn);
           turn = "o";
           compResponse("x");
         }
@@ -84,7 +105,7 @@ $(document).ready(function() {
           compWinComb = searchMatchNumber(compWinComb, currNum);
           $("#" + this.id).html("o");
 
-          findBlockingNumber(playerWinComb, playerArr, 1,turn);
+          findBlockingNumber(playerWinComb, playerArr, 1, turn);
           turn = "x";
           compResponse("o");
         }
@@ -104,7 +125,7 @@ $(document).ready(function() {
 
 //Function is responding with move , base on player move.
 // sPlayer - value is "x" or "o" , depending what was selected by player.
-function compResponse(sPlayer){
+function compResponse(sPlayer) {
   //When user will select center of board then use one from 4 comb numbers.
   this.selectedPlayer = sPlayer;
   if (currNum === 5 && playerArr.length == 1) {
@@ -114,10 +135,10 @@ function compResponse(sPlayer){
     var searchVal = searchMatchNumber(playerWinComb, Number(randNum));
     playerWinComb = searchVal;
     allNumbers[randNum] = 0;
-    if(this.selectedPlayer === "x"){
+    if (this.selectedPlayer === "x") {
       $("#squareNr" + randNum).html("o");
       turn = "x";
-    }else{
+    } else {
       $("#squareNr" + randNum).html("x");
       turn = "o";
     }
@@ -125,14 +146,14 @@ function compResponse(sPlayer){
   if (currNum != 5 && playerArr.length == 1) {
 
     // Level hard
-    if(selectedLevel ==="hard"){
+    if (selectedLevel === "hard") {
       var searchVal = searchMatchNumber(playerWinComb, Number(5));
       compArr.push(5);
       allNumbers[5] = 0;
-      if(this.selectedPlayer === "x"){
+      if (this.selectedPlayer === "x") {
         $("#squareNr" + 5).html("o");
         turn = "x";
-      }else{
+      } else {
         $("#squareNr" + 5).html("x");
         turn = "o";
       }
@@ -140,7 +161,7 @@ function compResponse(sPlayer){
     }
 
     // Easy level
-    if(selectedLevel ==="easy"){
+    if (selectedLevel === "easy") {
       var tempArr = allNumbers.filter(function(e) {
         if (e != 0) {
           return e
@@ -151,26 +172,26 @@ function compResponse(sPlayer){
       var searchVal = searchMatchNumber(playerWinComb, Number(randNum));
       allNumbers[randNum] = 0;
       playerWinComb = searchVal;
-        if(this.selectedPlayer === "x"){
-          $("#squareNr" + randNum).html("o");
-          turn = "x";
-        }else{
-          $("#squareNr" + randNum).html("x");
-          turn = "o";
-        }
+      if (this.selectedPlayer === "x") {
+        $("#squareNr" + randNum).html("o");
+        turn = "x";
+      } else {
+        $("#squareNr" + randNum).html("x");
+        turn = "o";
+      }
     }
   }
   if (playerArr.length >= 2) {
     var cBlock = findBlockingNumber(compWinComb, compArr, 0);
     var fBlock = findBlockingNumber(playerWinComb, playerArr, 0);
-    if(cBlock != undefined){
+    if (cBlock != undefined) {
       compArr.push(cBlock);
       allNumbers[cBlock] = 0;
       fBlock = cBlock;
       var searchVal = searchMatchNumber(playerWinComb, Number(cBlock));
       playerWinComb = searchVal;
-      findBlockingNumber(compWinComb, compArr,1);
-    }else{
+      findBlockingNumber(compWinComb, compArr, 1);
+    } else {
       if (fBlock != undefined) {
         compArr.push(fBlock);
         allNumbers[fBlock] = 0;
@@ -192,10 +213,10 @@ function compResponse(sPlayer){
       }
     }
 
-    if(this.selectedPlayer === "x"){
+    if (this.selectedPlayer === "x") {
       $("#squareNr" + fBlock).html("o");
       turn = "x";
-    }else{
+    } else {
       $("#squareNr" + fBlock).html("x");
       turn = "o";
     }
@@ -203,53 +224,53 @@ function compResponse(sPlayer){
 }
 
 //Function is drawing line when player or comp won the game
-function drawLine(arr){
+function drawLine(arr) {
 
   this.temp = arr;
   var a1 = document.getElementById("squareNr" + temp[0]);
   var b1 = a1.getBoundingClientRect();
   var linkLine = $('<div id="new-link-line"></div>').appendTo('body');
-  var cLength = Math.sqrt(Math.pow((b1.bottom-b1.top),2)+Math.pow((b1.right-b1.left),2));
-  var angle = (180/ Math.PI) * Math.acos((b1.bottom-b1.top)/cLength);
+  var cLength = Math.sqrt(Math.pow((b1.bottom - b1.top), 2) + Math.pow((b1.right - b1.left), 2));
+  var angle = (180 / Math.PI) * Math.acos((b1.bottom - b1.top) / cLength);
   if (temp[0] == 1 && temp[2] == 7 || temp[0] == 2 && temp[2] == 8 || temp[0] == 3 && temp[2] == 9) {
     linkLine
-    .css('top', b1.top)
-    .css('left', b1.left + ((b1.right-b1.left)/2)-10);
+      .css('top', b1.top)
+      .css('left', b1.left + ((b1.right - b1.left) / 2) - 10);
     $('#new-link-line')
-    .css('height', (b1.bottom - b1.top) * 3);
+      .css('height', (b1.bottom - b1.top) * 3);
   }
-  if(temp[0] == 1 && temp[2] == 3 || temp[0] == 4 && temp[2] == 6 || temp[0] == 7 && temp[2] == 9){
+  if (temp[0] == 1 && temp[2] == 3 || temp[0] == 4 && temp[2] == 6 || temp[0] == 7 && temp[2] == 9) {
     linkLine
-    .css('top', (b1.top)-15+((b1.bottom-b1.top)/2))
-    .css('left', b1.left);
+      .css('top', (b1.top) - 15 + ((b1.bottom - b1.top) / 2))
+      .css('left', b1.left);
     $('#new-link-line')
-    .css('height', 15)
-    .css('width',((b1.right-b1.left)*3));
+      .css('height', 15)
+      .css('width', ((b1.right - b1.left) * 3));
   }
-  if(temp[0] == 1 && temp[2] == 9){
+  if (temp[0] == 1 && temp[2] == 9) {
 
     linkLine
-    .css('top', b1.top)
-    .css('left', b1.left);
+      .css('top', b1.top)
+      .css('left', b1.left);
     $('#new-link-line')
-    .css('height', cLength * 3)
-    .css('-webkit-transform', 'rotate(' + (-angle) + 'deg)')
-    .css('-moz-transform', 'rotate(' + (-angle) + 'deg)')
-    .css('-o-transform', 'rotate(' + (-angle) + 'deg)')
-    .css('-ms-transform', 'rotate(' + (-angle) + 'deg)')
-    .css('transform', 'rotate(' + (-angle) + 'deg)');
+      .css('height', cLength * 3)
+      .css('-webkit-transform', 'rotate(' + (-angle) + 'deg)')
+      .css('-moz-transform', 'rotate(' + (-angle) + 'deg)')
+      .css('-o-transform', 'rotate(' + (-angle) + 'deg)')
+      .css('-ms-transform', 'rotate(' + (-angle) + 'deg)')
+      .css('transform', 'rotate(' + (-angle) + 'deg)');
   }
-  if(temp[0] == 3 && temp[2] == 7){
+  if (temp[0] == 3 && temp[2] == 7) {
     linkLine
-    .css('top', b1.top)
-    .css('left', b1.right-15);
+      .css('top', b1.top)
+      .css('left', b1.right - 15);
     $('#new-link-line')
-    .css('height', cLength * 3)
-    .css('-webkit-transform', 'rotate(' + angle + 'deg)')
-    .css('-moz-transform', 'rotate(' + angle + 'deg)')
-    .css('-o-transform', 'rotate(' + angle + 'deg)')
-    .css('-ms-transform', 'rotate(' + angle + 'deg)')
-    .css('transform', 'rotate(' + angle + 'deg)');
+      .css('height', cLength * 3)
+      .css('-webkit-transform', 'rotate(' + angle + 'deg)')
+      .css('-moz-transform', 'rotate(' + angle + 'deg)')
+      .css('-o-transform', 'rotate(' + angle + 'deg)')
+      .css('-ms-transform', 'rotate(' + angle + 'deg)')
+      .css('transform', 'rotate(' + angle + 'deg)');
   }
 
 }
@@ -258,7 +279,7 @@ function drawLine(arr){
 // arr - remaining win combinations array , pArr - array of numbers used by player.
 // opt - if 0 selected than fuction is finding blocking number
 // opt - if 1 selected than function is checking if user has winning combination
-function findBlockingNumber(arr, pArr, opt,turn) {
+function findBlockingNumber(arr, pArr, opt, turn) {
   this.combArr = arr;
   this.playerArrComb = pArr;
   this.turn = turn;
@@ -280,18 +301,19 @@ function findBlockingNumber(arr, pArr, opt,turn) {
         }
         drawLine(temp);
         allNumbers.fill(0);
-        if(this.turn === "x"){
+        if (this.turn === "x") {
           scoreX++;
-        }else if( this.turn === "o"){
+        } else if (this.turn === "o") {
           scoreO++;
-        }else if(this.turn == undefined){
-          if(player === "x"){
+        } else if (this.turn == undefined) {
+          if (player === "x") {
             scoreO++;
-          }else{
+          } else {
             scoreX++;
           }
         }
         $("#score").html("x   " + scoreX + " : " + scoreO + "   o");
+        $("#gameOptionsContainer").fadeIn(1500).removeClass("boardContainerHide");
       }
     }
   }
